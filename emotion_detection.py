1e3a3e76e0a4586c1e3cbef675787e094a07e023
tclapython <tclapython@gmail.com>
2024-08-02 09:20:51 -0500

    Task 2

---
+++ emotion_detection.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def emotion_detector(text_to_analyze):
+    """
+    function that analyzes text for emotion
+    """
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    my_object = {"raw_document": {"text": text_to_analyze}}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json=my_object, headers=header)
+    return response.text
